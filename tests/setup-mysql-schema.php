2674812ef03d19c62babd38dafb09500a9a936e8
Frank de Jonge <info@frenky.net>
2018-07-04 08:21:20 +0200

    Ise JSON field type

--- tests/setup-mysql-schema.php
+++ tests/setup-mysql-schema.php
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS domain_messages (
     event_type VARCHAR(100) NOT NULL,
     aggregate_root_id VARCHAR(36) NULL,
     time_of_recording DATETIME(6) NOT NULL,
-    payload TEXT NOT NULL,
+    payload JSON NOT NULL,
     INDEX aggregate_root_id (aggregate_root_id),
     INDEX time_of_recording (time_of_recording)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci ENGINE = InnoDB
